32bc83477a1e3338ddeb434264b28482f2c600cc
midudev <miduga@gmail.com>
2021-02-21 15:06:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+let notes = [
+  {
+    "id": 1,
+    "content": "Me tengo que suscribir a @midudev en YouTube",
+    "date": "2019-05-30T17:30:31.098Z",
+    "important": true
+  },
+  {
+    "id": 2,
+    "content": "Tengo que estudiar las clases del FullStack Bootcamp",
+    "date": "2019-05-30T18:39:34.091Z",
+    "important": false
+  },
+  {
+    "id": 3,
+    "content": "Repasar los retos de JS de midudev",
+    "date": "2019-05-30T19:20:14.298Z",
+    "important": true
+  }
+]
+
+const generateId = () => {
+  const notesIds = notes.map(n => n.id)
+  const maxId = notesIds.length ? Math.max(...notesIds) : 0
+  const newId = maxId + 1
+  return newId
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)  
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    return response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)  
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing'
+    })
+  }
+
+  const newNote = {
+    id: generateId(),
+    content: note.content,
+    date: new Date(),
+    import: note.important || false,
+  }
+
+  notes = notes.concat(newNote)
+
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
