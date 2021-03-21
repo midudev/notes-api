e0109995fa03c727376386cabaf6a17132fe1aef
midudev <miduga@gmail.com>
2021-03-21 14:54:54 +0100

    Add class

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String,
+  notes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Note'
+  }]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = model('User', userSchema)
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = User
